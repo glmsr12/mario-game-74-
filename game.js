0401771256b32f95c50abaeee87d79485e81923c
glmsr12 <gulumsercakmak@hotmail.com>
2023-04-12 07:27:01 -0700

    "update js file"

--- game.js
+++ game.js
@@ -1,24 +1,24 @@
 kaboom({
   global: true,
   fullscreen: true,
-  scale: 2,
+  scale: 1,
   debug: true,
   clearColor: [0, 0, 0, 1],
 });
 
-loadRoot('https://i.imgur.com/');
-loadSprite('coin', 'wbKxhcd.png');
-loadSprite('evil-shroom', 'KP03fR9.png');
-loadSprite('brick', 'pogC0x5.png');
-loadSprite('block', 'pdrLpi6.png');
-loadSprite('mario', 'Wb1qfhK.png');
-loadSprite('mushroom', '0wMd92p.png');
-loadSprite('suprise', 'gesQ1KP.png');
-loadSprite('unboxed', 'bdrLpi6.png');
-loadSprite('pipe-top-left', 'ReTPiWY.png');
-loadSprite('pipe-top-right', 'hj2GK4n.png');
-loadSprite('pipe-bottom-left', 'c1cYSbt.png');
-loadSprite('pipe-bottom-right', 'nqQ79eI.png');
+loadRoot('./images/');
+loadSprite('coin', '');
+loadSprite('evil-shroom', '');
+loadSprite('brick', 'brick.png');
+loadSprite('block', '');
+loadSprite('mario', '');
+loadSprite('mushroom', '');
+loadSprite('suprise', '');
+loadSprite('unboxed', '');
+loadSprite('pipe-top-left', '');
+loadSprite('pipe-top-right', '');
+loadSprite('pipe-bottom-left', '');
+loadSprite('pipe-bottom-right', '');
 
 scene('game', () => {
   layers(['bg', 'obj', 'ui'], 'obj');
